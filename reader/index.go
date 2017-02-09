1140ea3f461a85c337adca0a17b83d78cdb3f556
Tom <jiepengthegreat@126.com>
2017-02-09 19:08:51 +0800

    添加图片功能

--- reader/index.go
+++ reader/index.go
@@ -1,6 +1,14 @@
 package main
 
-import "golang.org/x/tour/reader"
+import (
+    "io"
+    "os"
+    "strings"
+)
+
+type rot13Reader struct {
+    r io.Reader
+}
 
 type MyReader struct{}
 
@@ -12,6 +20,20 @@ func (m MyReader) Read(b []byte) (int, error) {
 	return len(b), nil
 }
 
+func (rot *rot13Reader) Read(p []byte) (int, error) {
+    n, err := rot.r.Read(p)
+    for i := 0; i < len(p); i++ {
+        if (p[i] >= 'A' && p[i] < 'N') || (p[i] >='a' && p[i] < 'n') {
+            p[i] += 13
+        } else if (p[i] > 'M' && p[i] <= 'Z') || (p[i] > 'm' && p[i] <= 'z'){
+            p[i] -= 13
+        }
+    }
+    return n, err
+}
+
 func main() {
-	reader.Validate(MyReader{})
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+    r := rot13Reader
+    io.Copy(os.Stdout, &r)
 }
